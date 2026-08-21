31ce527ea488e9a5d575837b248159dfcec0e7e9
Ravi Wijerathna <thegamesploitrvf2@gmail.com>
2026-08-21 08:54:39 +0530

    Implement SimpleFlashLoan contract with flashLoan function

--- tr.sol
+++ tr.sol
@@ -1,11 +1,24 @@
-- name: convertToShares
-  type: function
-  stateMutability: view
+  pragma solidity 0.8.10;
 
-  inputs:
-    - name: assets
-      type: uint256
+  import "aave-v3-core/flashloan/base/FlashLoanSimpleReceiverBase.sol";
 
-  outputs:
-    - name: shares
-      type: uint256
+  contract SimpleFlashLoan is FlashLoanSimpleReceiverBase {
+
+    constructor(address _addressProvider) FlashLoanSimpleReceiverBase(IPoolAddressesProvider(_addressProvider)) {}
+
+    function flashLoan(address _token, uint256 _amount) public {
+      address receiverAddress = address(this); 
+      address asset = _token;
+      uint256 amount = _amount; 
+      bytes memory params = "";
+      uint16 referralCode = 0;
+
+      POOL.flashLoanSimple(receiverAddress, asset, amount, params, referralCode);
+    }
+
+    function executeOperation(address asset, uint256 amount, uint256 premium, address initiator, bytes calldata params) external override returns (bool) {
+      uint256 totalAmount = amount + premium; 
+      IERC20(asset).approve(address(POOL), totalAmount);
+      return true;
+    }
+  }
